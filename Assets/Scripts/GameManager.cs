e427e7ee9eb4289ee13382df6d47dcbe3061a85b
LiangGuanYu <153156766@qq.com>
2023-01-30 10:56:36 +0000

    11

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public UnityEvent OnPlayerDie = new UnityEvent();
+
+    public GameObject Player;
+    private void Awake()
+    {
+        if(instance==null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        OnPlayerDie.AddListener(OnPlayerDied);
+    }
+
+
+    void OnPlayerDied()
+    {
+        Player.gameObject.GetComponent<CharacterController>().enabled = false;  //Creazy
+        Player.transform.position = new Vector3(0, 3, -20);
+        Player.gameObject.GetComponent<CharacterController>().enabled = true;
+        Debug.Log(Player.transform.position);
+    }
+}
+
